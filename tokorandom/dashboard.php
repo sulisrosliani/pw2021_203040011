35197e2751fdebdcbe867e3557845fe1642428db
sulisrosliani <sulisrosliani1@gmail.com>
2021-05-19 04:13:05 +0700

    tubes pw

---
+++ tokorandom/dashboard.php
@@ -0,0 +1,52 @@
+<?php 
+    session_start();
+    if($_SESSION['status_login'] != true){
+        echo '<script>window.location="login.php"</script>';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tokorandom</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- header -->
+    <header>
+        <div class="container">
+            <h1><a href="dashboard.php">Toko Random</a></h1>
+            <ul>
+                <li><a href="dashboard.php">Dashboard</a></li>
+                <li><a href="profil.php">Profil</a></li>
+                <li><a href="data-kategori.php">Data Kategori</a></li>
+                <li><a href="data-produk.php">Data Produk</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <!-- content -->
+    <div class="section">
+        <div class="container">
+            <h3>Dashboard</h3>
+            <div class="box">
+                <h4>Selamat Datang <?php echo $_SESSION['a_global']->admin_name ?> di Toko Online</h4>
+            </div>
+        </div>
+    </div>
+
+    <!-- footer -->
+    <footer>
+        <div class="container">
+            <small>Copyright &copy; 2021 - Toko Random.</small>
+        </div>
+    </footer>
+    
+</body>
+</html>
